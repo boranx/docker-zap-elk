475597b62f91fdf425e78fd99b800412fc9a807e
Boran ŞEREF <boran.seref@gmail.com>
2017-07-28 10:14:48 +0300

    add polling for elastic search

--- run-docker.sh
+++ run-docker.sh
@@ -26,6 +26,7 @@ docker cp $CONTAINER_ID:zap/output.xml ./
 echo "ELK stack"
 git clone https://github.com/deviantony/docker-elk
 cp output.json docker-elk/ && cd docker-elk && docker-compose stop && docker-compose rm -f || /bin/true && docker-compose up -d
+until curl localhost:9200; do echo "Waiting for Elastic Search"; sleep 2; done
 
 echo "parse output.json - add indices"
 cat output.json | jq -c '.[] | {"index": {"_index": "bookmarks", "_type": "bookmark", "_id": .id}}, .' | curl -XPOST localhost:9200/_bulk --data-binary @-
